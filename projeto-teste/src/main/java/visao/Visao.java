00ab4b90119135348056e9f677e20de5b54edce5
Emily Elisabeth Neves <emily.en@aluno.ifsc.edu.br>
2023-08-01 08:58:12 -0300

    Primeira Aula
    PDS conexão

---
+++ projeto-teste/src/main/java/visao/Visao.java
@@ -0,0 +1,13 @@
+package visao;
+
+import controle.Conexao;
+
+public class Visao {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Conexao conectar = Conexao.getInstacia();
+	}
+
+}
