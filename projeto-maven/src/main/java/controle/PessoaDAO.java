74b220e461549cfb7562a460ad32b08d3cb13300
Emily Elisabeth Neves <emily.en@aluno.ifsc.edu.br>
2023-08-15 10:53:11 -0300

    Aula projeto-maven

---
+++ projeto-maven/src/main/java/controle/PessoaDAO.java
@@ -0,0 +1,78 @@
+package controle;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import modelo.Pessoa;
+
+public class PessoaDAO {
+
+	public ArrayList<Pessoa> listar() {
+
+		ConexaoBanco c = ConexaoBanco.getInstancia();
+
+		Connection con = c.conectar();
+
+		ArrayList<Pessoa> pessoas = new ArrayList<Pessoa>();
+		// Select Simples:
+		String query = "SELECT * FROM pessoa";
+		// Preparando a nossa query SQL acima em um obj java
+		PreparedStatement ps;
+		try {
+			ps = con.prepareStatement(query);
+
+			// Executa a nossa query de fato
+			ResultSet rs = ps.executeQuery();
+
+			while (rs.next()) {
+				// Pega o que o bd retornou
+				int idPessoa = rs.getInt("id_pessoa");
+				String primeiroNome = rs.getString("primeiro_nome");
+
+				// Cria um obj organizadinho de pessoa
+				Pessoa p1 = new Pessoa();
+				p1.setId_pessoa(idPessoa);
+				p1.setPrimeiro_nome(primeiroNome);
+
+				// Adiciona pessoa na lista:
+				pessoas.add(p1);
+
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		c.fecharConexao();
+		return pessoas;
+	}
+
+	public boolean inserir(Pessoa p) {
+
+		ConexaoBanco c = ConexaoBanco.getInstancia();
+
+		Connection con = c.conectar();
+
+		String query = "INSERT INTO pessoa " + "(id_pessoa, primeiro_nome) " + "VALUES (?, ?)";
+
+		try {
+			PreparedStatement ps = con.prepareStatement(query);
+
+			ps.setInt(1, p.getId_pessoa());
+			ps.setString(2, p.getPrimeiro_nome());
+
+			ps.executeUpdate();
+
+			c.fecharConexao();
+
+			return true;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
+
+}
---
+++ projeto-maven/src/main/java/modelo/Pessoa.java
@@ -0,0 +1,27 @@
+package modelo;
+
+public class Pessoa {
+	private int id_pessoa;
+	private String primeiro_nome;
+	private int idade;
+	public int getId_pessoa() {
+		return id_pessoa;
+	}
+	public void setId_pessoa(int id_pessoa) {
+		this.id_pessoa = id_pessoa;
+	}
+	public String getPrimeiro_nome() {
+		return primeiro_nome;
+	}
+	public void setPrimeiro_nome(String primeiro_nome) {
+		this.primeiro_nome = primeiro_nome;
+	}
+	public int getIdade() {
+		return idade;
+	}
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+	
+}
